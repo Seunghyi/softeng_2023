a99c651879bffb3cf1ca0fc6e2c53d689f08c651
KangSeunghee <125233056+Seunghyi@users.noreply.github.com>
2023-09-09 03:32:31 +0900

    Create 01_even.py

---
+++ week01/01_even.py
@@ -0,0 +1,10 @@
+# 홀짝 구분하기: 나머지 연산, 조건문
+def main():
+    a = 2
+    if a % 2 == 0:
+        print(f'{a}은/는 짝수입니다.')
+    else:
+        print(f'{a}은/는 홀수입니다.')
+
+if __name__ == "__main__":
+    main()
