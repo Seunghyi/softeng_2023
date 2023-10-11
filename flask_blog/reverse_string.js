bb41aac4868bf6569a4444897fcbea9030b96bc3
KangSeunghee <125233056+Seunghyi@users.noreply.github.com>
2023-10-11 10:24:51 +0900

    Delete flask_blog/reverse_string.js

--- flask_blog/reverse_string.js
+++
@@ -1,8 +0,0 @@
-function reverse() {
-
-    let str = document.getElementById('inputA').value;
-    document.getElementById("valueA").innerHTML = str;
-    document.getElementById("valueB").innerHTML = str.split('').reverse().join('');
-}
-
-
